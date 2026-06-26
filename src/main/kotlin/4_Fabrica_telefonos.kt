05a5fcca0f00bc3ced623b8c80fe904251a569dc
Miguel Perez Matia <mpmatia06@gmail.com>
2026-06-26 14:11:02 +0200

    Archivo del ejercicio 4 creado con enunciado

---
+++ src/main/kotlin/4_Fabrica_telefonos.kt
@@ -0,0 +1,6 @@
+'''
+    Crea una clase llamada Smartphone. Debe tener propiedades como marca, modelo y bateriaActual (de 0 a 100).
+    Luego, añádele un método (función) llamado usarApp() que reste 10 puntos de batería cada vez que se ejecute, e 
+    imprima un aviso si la batería baja de 20.
+    Crea un teléfono en tu fun main() y úsalo un par de veces. 
+'''
