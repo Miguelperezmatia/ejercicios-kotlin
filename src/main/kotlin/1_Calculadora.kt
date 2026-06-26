eea0eafa082fe050dde363987be4d79ee940d746
Miguel Perez Matia <mpmatia06@gmail.com>
2026-06-26 14:06:47 +0200

    Enunciado del primer ejercicio añadido

---
+++ src/main/kotlin/1_Calculadora.kt
@@ -0,0 +1,5 @@
+'''
+    Escribe un programa que simule la cuenta de un restaurante. Debes declarar una variable para el total de la cuenta, 
+    otra para el porcentaje de propina que quieres dejar, y calcular el total final a pagar.
+    Imprime el resultado en consola
+'''
