72ba768f08263c8657d66f947bd1b445d571cb48
Miguel Perez Matia <mpmatia06@gmail.com>
2026-06-26 14:09:46 +0200

    Archivo del ejercicio 3 creado con enunciado

---
+++ src/main/kotlin/3_Gestor_perfiles.kt
@@ -0,0 +1,5 @@
+'''
+    Escribe una función llamada mostrarPerfil que reciba dos parámetros: el nombre de un usuario (Texto) y su biografía (Texto).
+    El truco es que la biografía debe permitir ser nula (usando String?), simulando que el usuario no quiso escribir nada. 
+    Si la biografía es nula, imprime "Sin biografía", de lo contrario, imprime el texto.
+''' 
